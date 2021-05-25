18d4b518b7c93f9e5e23b753cf984d952dd9ef05
Li Lee <leesquare@gmail.com>
2021-05-25 14:38:40 -0600

    load users from API

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots'
 import './App.css'
 
 class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+    }
+
+    componentDidMount() {
+        console.log('check')
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(robots => this.setState({robots: robots}))
     }
 
     onSearchChange = (event) => {
@@ -20,20 +26,23 @@ class App extends Component {
     }
 
     render() {
+
         const filteredRobots = this.state.robots.filter(
             robot =>{
                 return robot.name.toLowerCase().includes(
                     this.state.searchfield.toLowerCase())}
         )
+        if (this.state.robots.length === 0) {
+            return <h1>Loading</h1>
+        } else {
         return (
-        <div className='tc'>
-        <h1 className='f1'> Robo Doggie Friends </h1>
-        <SearchBox searchChange={this.onSearchChange}/>
-        <CardList robots={filteredRobots} />    
+            <div className='tc'>
+            <h1 className='f1'> Robo Doggie Friends </h1>
+            <SearchBox searchChange={this.onSearchChange}/>
+            <CardList robots={filteredRobots} />    
+            </div>
 
-        </div>
-
-        )   
+        )}
     }
 }
 
