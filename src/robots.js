c06057ac477a747cd0519a2631586b762dc4b430
Li Lee <leesquare@gmail.com>
2021-05-25 12:45:04 -0600

    initial robofriends app

---
+++ src/robots.js
@@ -0,0 +1,39 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Linus',
+        username: 'linus_the_westie',
+        email: 'linus@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Barkley',
+        username: 'barkley_the_antiwestie',
+        email: 'barkley@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Millie',
+        username: 'millie_the_poodle',
+        email: 'millie@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Duncan',
+        username: 'duncan_the_tiny',
+        email: 'duncan@gmail.com'
+    },    
+    {
+        id: 5,
+        name: 'Archie',
+        username: 'archie_the_bernie',
+        email: 'archie@gmail.com'
+    },    
+    {
+        id: 6,
+        name: 'Pumpkin',
+        username: 'pumpkin_the_old',
+        email: 'pumpkin@gmail.com'
+    },
+
+]
