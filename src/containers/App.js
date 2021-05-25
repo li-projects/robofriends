2ac9427dc1e6cbe5c2af56fae2808e2bd438f4aa
Li Lee <leesquare@gmail.com>
2021-05-25 17:03:44 -0600

    add exception handling

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css'
 
 class App extends Component {
@@ -40,7 +41,10 @@ class App extends Component {
                 <h1 className='f1'> Robo Doggie Friends </h1>
                 <SearchBox searchChange={this.onSearchChange}/>
                 <Scroll>
-                    <CardList robots={filteredRobots} /> 
+                    <ErrorBoundary>
+                        <CardList robots={filteredRobots} /> 
+                    </ErrorBoundary>
+                    
                 </Scroll>
                 
                 </div>
